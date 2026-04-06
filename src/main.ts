86946c9bcaea7011cbea19ae29b9aa74d979d54d
Sevinch <askarbekovasevinc@gmail.com>
2026-04-06 09:40:05 +0500

    Xatolar tuzatildi

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: process.env.FRONTEND_URL || '*',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
+  // app.enableCors({
+  //   origin: process.env.FRONTEND_URL || '*',
+  //   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  // });
 
   app.useGlobalPipes(
     new ValidationPipe({
